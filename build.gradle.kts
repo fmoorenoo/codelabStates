ae95b9942b0a5c0da06dfc2c4cb9fcebdfa5b557
fmoorenoo <fermontemore@gmail.com>
2024-11-29 08:27:32 +0000

    Dependencias añadidas en build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,15 @@ repositories {
 }
 
 dependencies {
+    implementation(compose.material3)
     // Note, if you develop a library, you should use compose.desktop.common.
     // compose.desktop.currentOs should be used in launcher-sourceSet
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.8.1")
+
 }
 
 compose.desktop {
