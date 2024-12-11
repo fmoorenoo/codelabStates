6e9fe4506ef934c2b6c60745be6db89bac491877
fmoorenoo <fermontemore@gmail.com>
2024-12-11 08:26:36 +0000

    WaterCounter modificado

--- src/main/kotlin/WaterCounter.kt
+++ src/main/kotlin/WaterCounter.kt
@@ -1,45 +1,32 @@
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-
+import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.padding(8.dp),
-        horizontalAlignment = Alignment.Start
-    ) {
-        var contadorVasos by remember { mutableStateOf(0) }
-        if (contadorVasos > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { },
-                    taskName = "Es importante beber mucha agua"
-                )
-            }
-            Text("Tienes $contadorVasos vasos.")
+fun WaterCounter() {
+    var counter by rememberSaveable() { mutableStateOf(0) }
+    Column {
+        if (counter > 0){
+            val vasosTexto = if (counter == 1 ) "vaso" else "vasos"
+            Text("Hay ${counter} ${vasosTexto} de agua")
         }
-        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+        Row {
             Button(
-                onClick = { contadorVasos++ },
-                Modifier.padding(vertical = 15.dp),
-                enabled = contadorVasos < 10
+                onClick = {
+                    counter++
+                }
             ) {
-                Text("Añadir vaso")
+                Text("Añadir")
             }
             Button(
-                onClick = { contadorVasos = 0 }
-            )
-            {
-                Text("Reset")
+                onClick = {
+                    counter = 0
+                }
+            ) {
+                Text("Reiniciar")
             }
         }
     }
