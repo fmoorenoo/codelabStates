867083e83c1115d6e36377275bd4592423ebc583
fmoorenoo <fermontemore@gmail.com>
2024-12-11 08:33:39 +0000

    WellnessScreen corregido

--- src/main/kotlin/WellnessScreen.kt
+++ src/main/kotlin/WellnessScreen.kt
@@ -1,16 +1,14 @@
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(
     modifier: Modifier = Modifier,
-    wellnessViewModel: EjemploViewModel = viewmodel()
+    wellnessViewModel: EjemploViewModel = viewModel()
 ) {
     WellnessTaskList(
-        list = wellnessViewModel.tasks,
+        listaItems = wellnessViewModel.tasks,
         onCheckedTask = { task, checked ->
             wellnessViewModel.changeTaskChecked(task, checked)
         },
