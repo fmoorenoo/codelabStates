7b1b21afe7a2194b33669c3222ee6c7cae0a5141
fmoorenoo <fermontemore@gmail.com>
2024-12-11 09:59:18 +0000

    WellnessTaskItem.kt modificado

--- src/main/kotlin/WellnessTaskItem.kt
+++ src/main/kotlin/WellnessTaskItem.kt
@@ -1,29 +1,37 @@
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
 fun WellnessTaskItem(
-    taskName: String,
-    onClose: () -> Unit,
+    task: WellnessTask,
+    onClose: (task: WellnessTask) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val checkedState = rememberSaveable(){ mutableStateOf(true) }
     Row(
         modifier = modifier, verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             modifier = Modifier.weight(1f).padding(start = 16.dp),
-            text = taskName
+            text = task.label
         )
-        IconButton(onClick = onClose) {
+        Checkbox(
+            checked = checkedState.value,
+            onCheckedChange = {checkedState.value = it}
+        )
+        IconButton(onClick = { onClose(task) }) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
